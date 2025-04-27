e2a70a07a518d7d68073506d7decab8505889fc3
{{your_name}} <{{your_email}}>
2025-04-27 19:48:02 +0600

    project initialized

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from 'antd'
+
+function App() {
+  return (
+    <>
+      <h1 className='text-5xl'>Dynamic Employee Table</h1>
+      <Button type='primary'>Hello</Button>
+    </>
+  )
+}
+
+export default App
